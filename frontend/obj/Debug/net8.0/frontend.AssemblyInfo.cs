3b210797ca89aaa47f2b5d9e22bb72976338490e
Jasmin <05jasmay@gmail.com>
2026-06-23 13:14:03 +0000

    What: Implemented template for home page. Why. So that I have a starting point

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed87b846f79ec59f0bb193ea148f48def2d9bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891952d00a683f3574c72a2c272705c741caa629")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
