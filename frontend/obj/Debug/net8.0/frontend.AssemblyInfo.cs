a3e9894ae6158d6b9167d97f71a208abf3e1ef4f
Jasmin <05jasmay@gmail.com>
2026-06-25 06:54:17 +0000

    Clean build artifacts from main

--- frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b210797ca89aaa47f2b5d9e22bb72976338490e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36ca6f8b0298789aa560f7cf1321648aae30cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
